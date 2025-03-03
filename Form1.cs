599e13a297e018ef193238c3aba6fc1dbeaf3592
shintaaulya <shintaaulya20@gmail.com>
2025-03-04 00:20:39 +0700

    perubahan kode

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace tpMod3
 {
@@ -15,6 +16,31 @@ namespace tpMod3
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nama = textBox1.Text.Trim();
+
+            if (!string.IsNullOrEmpty(nama))
+            {
+                textBox1.Text = nama;
+                MessageBox.Show("Halo " + nama, "Salam", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Nama tidak boleh kosong!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
